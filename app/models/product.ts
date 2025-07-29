c9d6123275139410da5ba8da79132a29dc647c61
TigorBekerja <120919621+TigorBekerja@users.noreply.github.com>
2025-07-29 09:21:25 +0700

    feat(talent-growth): add product
    product model and migration

---
+++ app/models/product.ts
@@ -0,0 +1,33 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from '#models/user'
+
+export default class Product extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare description?: string
+
+  @column()
+  declare type: string
+
+  @column()
+  declare price: number
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
